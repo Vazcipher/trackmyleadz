8774c9a6a1dd00b018cd8a89983fbe9a264e807f
sidan123 <sidan@baabte.com>
2019-09-12 11:19:27 +0530

    service for create and get enquiry

--- trackapp/views.py
+++ trackapp/views.py
@@ -62,6 +62,7 @@ def enquires(request):
 	except Exception as identifier:
 		print(identifier)
 
+
 def consumer(request):
 	try:
 		user_obj = UserLogin.objects.get(id=request.session['userId'])
@@ -72,6 +73,7 @@ def consumer(request):
 	except Exception as identifier:
 		print(identifier)
 
+
 def employee(request):
 	try:
 		user_obj = UserLogin.objects.get(id=request.session['userId'])
@@ -81,6 +83,7 @@ def employee(request):
 		return render(request, 'employee.html', context)
 	except Exception as identifier:
 		print(identifier)
+
 
 def others(request):
 	try:
@@ -103,6 +106,7 @@ def reports(request):
 	except Exception as identifier:
 		print(identifier)
 
+
 def charts(request):
 	try:
 		user_obj = UserLogin.objects.get(id=request.session['userId'])
@@ -110,5 +114,50 @@ def charts(request):
 			"username": user_obj.username
 		}
 		return render(request, 'charts.html', context)
+	except Exception as identifier:
+		print(identifier)
+
+
+def fn_create_enquiry(request):
+	try:
+		if request.method == 'POST':
+
+			lead_title = request.POST['title']
+			lead_source = request.POST['source']
+			lead_stage = request.POST['stage']
+
+			first_name = request.POST['firstname']
+			last_name = request.POST['lastname']
+			product = request.POST['product']
+			phone_number = request.POST['number']
+			email = request.POST['email']
+			address = request.POST['address']
+			gender = request.POST['gender']
+			desc = request.POST['desc']
+			
+			user_obj = UserLogin.objects.get(id=request.session['userId'])
+			company_obj = Company(id=request.session['companyId'])
+			ass_user_obj = UserLogin.objects.get(id=request.POST['assigneduser'])
+
+			lead_obj = Leads(fk_created_user_id=user_obj, fk_updated_user_id=user_obj, fk_company_id=company_obj,
+			 				 fk_assigned_user_id=ass_user_obj, lead_title=lead_title, lead_source=lead_source, lead_stage=lead_stage)
+			lead_obj.save()
+
+			if lead_obj.id > 0:
+				lead_detail_obj = LeadDetails(fk_lead_id=lead_obj, first_name=first_name, last_name=last_name, product=product,
+										  	  phone_number=phone_number, email=email, address=address, gender=gender, description=desc)
+				lead_detail_obj.save()
+				
+				return HttpResponse('created')
+			return HttpResponse('failed')
+
+	except Exception as identifier:
+		print(identifier)
+
+
+def fn_get_enquiry(request):
+	try:
+		company_obj = Company(id=request.session['companyId'])
+		leads = Leads.objects.filter(fk_company_id=company_obj)
 	except Exception as identifier:
 		print(identifier)
