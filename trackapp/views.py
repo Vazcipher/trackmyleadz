f938ad5015064c3f4b5c4d971257d78a820c6880
Anusha754 <anushanageri@gmail.com>
2019-09-25 15:05:47 +0530

    view employee

--- trackapp/views.py
+++ trackapp/views.py
@@ -92,9 +92,11 @@ def employee(request):
         user_obj = UserLogin.objects.get(id=request.session['userId'])
         company_obj = Company.objects.get(id=request.session['companyId'])
         role_obj = UserRole.objects.filter(fk_company_id=company_obj)
+        userdet_obj = UserDetails.objects.filter(fk_login_id__fk_company_id=company_obj)
         context = {
             "username": user_obj.username,
-            "roles": role_obj
+            "roles": role_obj,
+            "userdetail":userdet_obj
         }
         return render(request, 'employee.html', context)
     except Exception:
