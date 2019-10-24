d05a7e3541d6d965ab7688446d950c78b37742f4
sajitha859 <sajithaazees11@gmail.com>
2019-10-24 13:48:44 +0530

    for merge

--- trackapp/static/js/enquires.js
+++ trackapp/static/js/enquires.js
@@ -114,6 +114,9 @@ function fn_save_enquiery() {
                 $('#product').val('');
                 $('#assigned').val('');
                 $('#des').val('');
+                setTimeout(() => {
+                    location.reload(true);
+                }, 1500)
             },
             error: e => {
                 $.toast({
@@ -150,13 +153,16 @@ function fn_delete_enquiry(lead_id) {
                 icon: 'success',
                 showHideTransition: 'fade',
                 allowToastClose: true,
-                hideAfter: 2000,
+                hideAfter: 1500,
                 stack: 5,
                 position: 'top-right',
                 textAlign: 'left',
                 loader: true,
                 loaderBg: '#9EC600'
             });
+            setTimeout(() => {
+                location.reload(true);
+            }, 1500)
         },
         error: e => {
             $.toast({
@@ -176,4 +182,65 @@ function fn_delete_enquiry(lead_id) {
 
     });
 
+}
+
+function fn_save_lead_source() {
+    $.ajax({
+        url: 'http://127.0.0.1:8000/trackapp/createLeadSource/',
+        type: 'POST',
+        data: {
+            title: $('#source_title').val(),
+            desc: $('#source_desc').val()
+        },
+        success: res => {
+            if (res.status) {
+                $('#addLeadSource').modal('hide');
+                $.toast({
+                    text: 'New lead source added',
+                    heading: 'Note',
+                    icon: 'success',
+                    showHideTransition: 'fade',
+                    allowToastClose: true,
+                    hideAfter: 1500,
+                    stack: 5,
+                    position: 'top-right',
+                    textAlign: 'left',
+                    loader: true,
+                    loaderBg: '#9EC600'
+                });
+                $('#lead_source').append(
+                    "<option value=" + res.id + ">" + res.title + "</option>"
+                )
+            } else {
+                $.toast({
+                    text: 'Failed to add new lead source',
+                    heading: 'Note',
+                    icon: 'error',
+                    showHideTransition: 'fade',
+                    allowToastClose: true,
+                    hideAfter: 3000,
+                    stack: 5,
+                    position: 'top-right',
+                    textAlign: 'left',
+                    loader: true,
+                    loaderBg: '#9EC600',
+                });
+            }
+        },
+        error: e => {
+            $.toast({
+                text: e,
+                heading: 'Note',
+                icon: 'error',
+                showHideTransition: 'fade',
+                allowToastClose: true,
+                hideAfter: 3000,
+                stack: 5,
+                position: 'top-right',
+                textAlign: 'left',
+                loader: true,
+                loaderBg: '#9EC600',
+            });
+        }
+    })
 }
