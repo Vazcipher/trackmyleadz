e3c1ab1dfd3f2af6897576fc3f3f4c3cbd931912
theerthakp <theerthakp95@gmail.com>
2019-10-28 09:48:22 +0530

    view change

--- trackapp/views.py
+++ trackapp/views.py
@@ -517,7 +517,7 @@ def fn_edit_consumer(req):
         user_obj = UserLogin.objects.get(id=request.session['userId'])
         company_obj = Company.objects.get(id=request.session['companyId'])
         cons_obj = Consumer.objects.filter(fk_company_id=company_obj)
-        if req.method == 'POST'
+        if req.method == 'POST':
             consumer.object.filter(id=req.POST['consumer_id']).update(fistname=req.POST['fistname'],lastname=req.POST['lastname'],email=req.POST['email'],phone=req.POST['phone'],address=req.POST['address'],gender=req.POST['gender'])
             return HttpResponse('consumer updated')
         consumer_obj=Consumer.object.get(consumer_id=req.GET['id'])
